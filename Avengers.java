c9c16a937df78da395ef11e7ca82cc4408a07aed
Rabbit <rohanp.xworkz@gmail.com>
2022-09-11 18:12:01 +0530

    Basic Program

---
+++ Avengers.java
@@ -0,0 +1,48 @@
+class Avengers{
+	
+	public static void main(String[] args)
+	{
+		int[] doorNos={55,45,65,78,12,36,47,98,64,99};
+		System.out.println("total="+doorNos.length);
+		
+
+		for(int rob=0;rob<doorNos.length;rob++)
+		{
+			int ref=doorNos[rob];
+			System.out.println(ref);
+		}
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
