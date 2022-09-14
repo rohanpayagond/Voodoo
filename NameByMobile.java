a5bad755bb0a4696643b76c1b5835dee772d2979
Rabbit <rohanp.xworkz@gmail.com>
2022-09-14 23:06:02 +0530

    method program

---
+++ NameByMobile.java
@@ -0,0 +1,71 @@
+class NameByMobile{
+	
+	static String getNameByMobile(long number)
+	{
+		System.out.println("Entered getNameByMobile");
+		if(number==9884765749L)
+		{
+			System.out.println("Arjun mobile number is 9884765749L");
+			return "Arjun";
+		}
+		
+		if(number==9745887698L)
+		{
+			System.out.println("Suman mobile number is 9745887698L");
+			return "Suman";
+		}
+		
+		if(number==9986234758L)
+		{
+			System.out.println("Sanjay mobile number is 9986234758L");
+			return "Sanjay";
+		}
+		
+		if(number==9845675864L)
+		{
+			System.out.println("Akash mobile number is 9845675864L");
+			return "Akash";
+		}
+		
+		if(number==9845786897L)
+		{
+			System.out.println("Sumit mobile number is 9845786897L");
+			return "Sumit";
+		}
+		
+		if(number==9987035687L)
+		{
+			System.out.println("Krishna mobile number is 9987035687L");
+			return "Krishna";
+		}
+		
+		if(number==9632609879L)
+		{
+			System.out.println("Manoj mobile number is 9632609879L");
+			return "Manoj";
+		}
+		
+		if(number==9877694859L)
+		{
+			System.out.println("Aniruddh mobile number is 9877694859L");
+			return "Aniruddh";
+		}
+		
+		if(number==9825689576L)
+		{
+			System.out.println("Gagan mobile number is 9825689576L");
+			return "Gagan";
+		}
+		
+		if(number==9878699978L)
+		{
+			System.out.println("Ashish mobile number is 9878699978L");
+			return "Ashish";
+		}
+		
+	return null;
+	}
+	
+
+	
+}
---
+++ NameByMobileRunner.java
@@ -0,0 +1,11 @@
+class NameByMobileRunner{
+	
+	public static void main(String[] mobileno)
+	{
+		String name=NameByMobile.getNameByMobile(9878699978L);
+		System.out.println(name);
+	}
+	
+	
+	
+}
