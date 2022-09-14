03bceeb4071267f2339bb7bffb6ad49e0adff6d5
Rabbit <rohanp.xworkz@gmail.com>
2022-09-14 23:08:30 +0530

    method program

---
+++ CapitalByState.java
@@ -0,0 +1,185 @@
+class CapitalByState{
+	
+	static String getCapitalByState(String state)
+	{
+		System.out.println("Entered getCapitalByState");
+		if(state=="Karnataka")
+		{
+			System.out.println("Bengaluru is the capital of Karnataka");
+			return "Bengaluru";
+		}
+		
+		if(state=="Kerala")
+		{
+			System.out.println("Thiruvananthapuram is the capital of Kerala");
+			return "Thiruvananthapuram";
+		}
+		
+		if(state=="Goa")
+		{
+			System.out.println("Panaji is the capital of Goa");
+			return "Panaji";
+		}
+		
+		if(state=="Rajasthan")
+		{
+			System.out.println("Jaipur is the capital of Rajasthan");
+			return "Jaipur";
+		}
+		
+		if(state=="UttarPradesh")
+		{
+			System.out.println("Lucknow is the capital of UttarPradesh");
+			return "Lucknow";
+		}
+		
+		if(state=="Maharashtra")
+		{
+			System.out.println("Mumbai is the capital of Maharashtra");
+			return "Mumbai";
+		}
+		
+		if(state=="Punjab")
+		{
+			System.out.println("Chandigarh is the capital of Punjab");
+			return "Chandigarh";
+		}
+		
+		if(state=="Bihar")
+		{
+			System.out.println("Patna is the capital of Bihar");
+			return "Patna";
+		}
+		
+		if(state=="TamilNadu")
+		{
+			System.out.println("Chennai is the capital of TamilNadu");
+			return "Chennai";
+		}
+		
+		if(state=="Gujarat")
+		{
+			System.out.println("Gandhinagar is the capital of Gujarat");
+			return "Gandhinagar";
+		}
+		
+		if(state=="MadhyaPradesh")
+		{
+			System.out.println("Bhopal is the capital of MadhyaPradesh");
+			return "Bhopal";
+		}
+		
+		if(state=="Haryana")
+		{
+			System.out.println("Chandigarh is the capital of Haryana");
+			return "Chandigarh";
+		}
+		
+		if(state=="Telangana")
+		{
+			System.out.println("Hyderabad is the capital of Telangana");
+			return "Hyderabad";
+		}
+		
+		if(state=="Assam")
+		{
+			System.out.println("Dispur is the capital of Assam");
+			return "Dispur";
+		}
+		
+		if(state=="Jharkhand")
+		{
+			System.out.println("Ranchi is the capital of Jharkhand");
+			return "Ranchi";
+		}
+		
+		if(state=="Uttarakhand")
+		{
+			System.out.println("Dehradun is the capital of Uttarakhand");
+			return "Dehradun";
+		}
+		
+		if(state=="WestBengal")
+		{
+			System.out.println("Kolkata is the capital of WestBengal");
+			return "Kolkata";
+		}
+		
+		if(state=="Odisha")
+		{
+			System.out.println("Bhubaneswar is the capital of Odisha");
+			return "Bhubaneswar";
+		}
+		
+		if(state=="Chattisgarh")
+		{
+			System.out.println("Raipur is the capital of Chattisgarh");
+			return "Raipur";
+		}
+		
+		if(state=="HimachalPradesh")
+		{
+			System.out.println("Shimla is the capital of HimachalPradesh");
+			return "Shimla";
+		}
+		
+		if(state=="Nagaland")
+		{
+			System.out.println("Kohima is the capital of Nagaland");
+			return "Kohima";
+		}
+		
+		if(state=="Sikkim")
+		{
+			System.out.println("Gangtok is the capital of Sikkim");
+			return "Gangtok";
+		}
+		
+		
+		if(state=="Manipur")
+		{
+			System.out.println("Imphal is the capital of Manipur");
+			return "Imphal";
+		}
+		
+		
+		if(state=="Meghalya")
+		{
+			System.out.println("Shillong is the capital of Meghalya");
+			return "Shillong";
+		}
+		
+		
+		if(state=="ArunachalPradesh")
+		{
+			System.out.println("Itanagar is the capital of ArunachalPradesh");
+			return "Itanagar";
+		}
+		
+		if(state=="Tripura")
+		{
+			System.out.println("Agartala is the capital of Tripura");
+			return "Agartala";
+		}
+		
+		
+		if(state=="Mizoram")
+		{
+			System.out.println("Aizawl is the capital of Mizoram");
+			return "Aizawl";
+		}
+		
+		if(state=="AndhraPradesh")
+		{
+			System.out.println("Amaravati is the capital of AndhraPradesh");
+			return "Amaravati";
+		}
+		
+		
+		return null;
+	}
+
+	
+
+	
+}
---
+++ CapitalByStateRunner.java
@@ -0,0 +1,13 @@
+class CapitalByStateRunner{
+	
+	public static void main(String[] capital)
+	{
+		String ref=CapitalByState.getCapitalByState("Uttarakhand");
+		System.out.println(ref);
+		
+		
+
+	}
+	
+	
+}
