71f396e39da5167dbcfb00931685e113f6636113
Rabbit <rohanp.xworkz@gmail.com>
2022-09-19 00:44:17 +0530

    method

---
+++ Person.java
@@ -0,0 +1,23 @@
+class Person{
+	
+	static void work(String company,int salary)
+	{
+		System.out.println("Entered Company And Salary");
+		System.out.println("Name of the company= "+company);
+		System.out.println("Company Salary= "+salary);
+	}
+	
+	
+	static void work(String company,int salary,String designation)
+	{
+		System.out.println("Entered Comapny,Salary And Designation");
+		System.out.println("Name of the company= "+company);
+		System.out.println("Company Salary= "+salary);
+		System.out.println("Designation is= "+designation);
+	}
+	
+	
+	
+	
+	
+}
---
+++ PersonRunner.java
@@ -0,0 +1,25 @@
+class PersonRunner{
+	
+	public static void main(String[] task)
+	{
+	Person.work("Accenture",15000);
+	Person.work("Accenture",15000,"Developer Trainee");
+	
+	Person.work("TCS",25000);
+	Person.work("TCS",25000,"Web Developer");
+	
+	Person.work("Infosys",30000);
+	Person.work("Infosys",30000,"Junior Software Developer");
+	
+	Person.work("Cisco",35000);
+	Person.work("Cisco",35000,"Senior Software Developer");
+	
+	Person.work("Mindtree",55000);
+	Person.work("Mindtree",55000,"HR");
+	
+	}
+	
+	
+	
+	
+}
