557635784dfb062ccf3ee1a87604aa6322e500bd
Rabbit <rohanp.xworkz@gmail.com>
2022-09-19 00:40:29 +0530

    method

---
+++ TransportRunner.java
@@ -0,0 +1,14 @@
+class TransportRunner{
+	
+ public static void main(String[]travel){
+	 
+	double distance=Transport.travelling("Hubli","Bangalore");
+	System.out.println("distance= "+distance);
+	
+	
+
+ }
+ 
+ 
+ 
+ }
